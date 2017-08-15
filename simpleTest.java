a4d6cd6f68d9e5ae855d13e5e0d130c0c0c98cce
Aurélio Buarque <abuarquemf@gmail.com>
2017-08-15 00:19:39 -0300

    added simple test

---
+++ simpleTest.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class Main {
+
+    //emotions menu
+    private static String[] emotions = {"anger", "joy",
+            "neutral", "fear", "disgust", "sadness", "surprise"};
+
+    public static void fetchServer() throws Exception {
+        EmotionServerHandler esh = EmotionServerHandler.getInstance();
+
+        Random randomInt = new Random();
+        //iterating through the emotions menu 10 times and getting random emotions
+        for(int j = 0; j < 20; j++) {
+            int index = randomInt.nextInt(7);
+            esh.sendEmotionToServer(emotions[index], 100);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        fetchServer();
+    }
+}
